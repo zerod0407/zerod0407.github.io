f86b599fdac71c4bc55550966dda1f655c23ffb8
zerod0407 <zerod0407@gmail.com>
2022-12-19 10:45:47 +0800

    1219-1

--- form5.js
+++ form5.js
@@ -1,8 +1,9 @@
+
 /*=====================================
   ex
 =====================================*/
 function test(){
-  let item ={
+  let item = {
     width: '3',
     label: '姓名',
     type: "text",
@@ -11,8 +12,42 @@ function test(){
     valid: "required",
     option: "單選1|單選2|單選3",
   };
-  let form_item = [{},{},{}]
+  for(let i in item){
+    console.log(i + ': ',item[i]);
+  }
 
-  console.log(item.width, item['label']);
+  let form_item = [
+    {
+      width: '3',
+      label: '姓名',
+      type: "text",
+      name: "name",
+      value: "",
+      valid: "required",
+      option: "",
+  },
+  {
+      width: '3',
+      label: '網址',
+      type: "url",
+      name: "url",
+      value: "",
+      valid: "required",
+      option: "",
+  },
+  {
+      width: '3',
+      label: 'EMAIL',
+      type: "email",
+      name: "email",
+      value: "",
+      valid: "",
+      option: "",
+  }]
+
+
+  // console.log(item.width, item['label']);
+  // console.log(form_item[0].label);
+  // console.log(form_item[0]['label']);
 
 }
